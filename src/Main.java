6c0a873bd907b37b67d8ca18a8baae85a23eaa2d
Wenfei Wang <395110618@qq.com>
2022-01-09 19:03:00 +0800

    增加题目

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import leetcode.LeetCode64;
+import leetcode.LeetCode976;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        int[][] grid={{0}};
-        int res=new LeetCode64().minPathSum(grid);
-        System.out.println("res = " + res);
+        int[] nums={3,6,2,3};
+        int res=new LeetCode976().largestPerimeter(nums);
+        System.out.println(res);
     }
 }
---
+++ src/leetcode/LeetCode976.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class LeetCode976 {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=nums.length-1;i>=2;i--){
+            if(nums[i]>=nums[i-1]+nums[i-2]){
+                continue;
+            }else{
+                return (nums[i]+nums[i-1]+nums[i-2]);
+            }
+        }
+        return 0;
+    }
+}
